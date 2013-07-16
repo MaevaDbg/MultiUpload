1c3ead04ae1d789db7f6dfa902b2ba39f48c439e
Maeva Dubourg <maeva.dbg@gmail.com>
2013-07-16 15:57:06 +0200

    Mise en place MaDevUploadFilebundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PunkAve\FileUploaderBundle\PunkAveFileUploaderBundle(),
             new Mav\TestUploadBundle\MavTestUploadBundle(),
+            new MaDev\UploadFileBundle\MaDevUploadFileBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
